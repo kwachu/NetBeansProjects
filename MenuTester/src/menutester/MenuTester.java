f7461cb1b107aed424836f733a5a5c8cdd38ec8e
kwachu <kwachu@kwachu.pl>
2013-03-23 19:23:31 +0100

    z menu testrem;)

---
+++ MenuTester/src/menutester/MenuTester.java
@@ -0,0 +1,36 @@
+/*
+ * test :)
+ */
+package menutester;
+
+import java.util.Scanner;
+/**
+ *
+ * @author kwachu
+ */
+public class MenuTester {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int choice=1;
+        Scanner reader = new Scan
+        
+        while (true) {
+            System.out.print("***********MENU*************"
+                    + "Wybierz opcje:"
+                    + "1. Pokaz baze"
+                    + "2. Dodaj wpis do bazy"
+                    + "3. unter Konstrukzionierung");
+            
+            
+            switch(choice) {
+                
+            }
+             
+        
+        }
+    }
+}
