19373ac3a34069fd02ca3acdbb04fbb4fd0fdd5a
kwachu <kwachu@kwachu.pl>
2013-03-24 01:04:47 +0100

    menu tester working

--- MenuTester/src/menutester/MenuTester.java
+++ MenuTester/src/menutester/MenuTester.java
@@ -4,6 +4,7 @@
 package menutester;
 
 import java.util.Scanner;
+import java.io.IOException;
 /**
  *
  * @author kwachu
@@ -13,24 +14,43 @@ public class MenuTester {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
-        int choice=1;
-        Scanner reader = new Scan
+        int choice =1;
+        Scanner reader = new Scanner(System.in);
+        //IOException ioe;
         
-        while (true) {
-            System.out.print("***********MENU*************"
-                    + "Wybierz opcje:"
-                    + "1. Pokaz baze"
-                    + "2. Dodaj wpis do bazy"
-                    + "3. unter Konstrukzionierung");
-            
+        while(choice != 0)  {
+            try { 
+                Runtime.getRuntime().exec("echo duap");
+            } 
+            catch (IOException ioe) {
+                System.err.print(ioe);
+            }
+                
+            System.out.print("***********MENU*************\n"
+                    + "Wybierz opcje:\n"
+                    + "1. Pokaz baze\n"
+                    + "2. Dodaj wpis do bazy\n"
+                    + "3. unter Konstrukzionierung\n");
+            choice = Integer.parseInt(reader.next());
             
             switch(choice) {
-                
-            }
-             
+                case 1: {
+                    System.out.println("Wybrano 1");
+                    break;
+                }
+                case 2: {
+                    System.out.println("Wybrano 2");
+                    break;
+                }
+                default: {
+                    System.out.println("Wybrano nieobslugiwana opcje");
+                }    
+
         
         }
+    }
+
     }
 }
